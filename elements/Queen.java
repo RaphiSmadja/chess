8d43eac8e6cda12bc84e9a5747e0db2056b566d2
Rayan2110 <rayan.kallal54@yahoo.fr>
2023-10-24 19:34:47 +0200

    CHANGEMENT

--- elements/Queen.java
+++ elements/Queen.java
@@ -11,6 +11,24 @@ public class Queen extends Piece {
 
     @Override
     public boolean isValidMove(Position newPosition, Cell[][] board) {
+        int currentRow = getPosition().getRow();
+        int currentCol = getPosition().getColumn();
+        int newRow = newPosition.getRow();
+        int newCol = newPosition.getColumn();
+
+        int rowDiff = Math.abs(newRow - currentRow);
+        int colDiff = Math.abs(newCol - currentCol);
+
+        // Vérifie le mouvement en ligne droite (comme la Tour)
+        if ((rowDiff == 0 && colDiff > 0) || (rowDiff > 0 && colDiff == 0)) {
+            return isStraightPathClear(currentRow, currentCol, newRow, newCol, board);
+        }
+
+        // Vérifie le mouvement en diagonale (comme le Fou)
+        if (rowDiff == colDiff) {
+            return isDiagonalPathClear(currentRow, currentCol, newRow, newCol, board);
+        }
+
         return false;
     }
 
@@ -18,5 +36,41 @@ public class Queen extends Piece {
     public String toString() {
         return "Q";
     }
+    private boolean isStraightPathClear(int startRow, int startCol, int endRow, int endCol, Cell[][] board) {
+        int rowDirection = (endRow - startRow) / Math.abs(endRow - startRow);
+        int colDirection = (endCol - startCol) / Math.abs(endCol - startCol);
 
+        int checkRow = startRow + rowDirection;
+        int checkCol = startCol + colDirection;
+
+        while (checkRow != endRow || checkCol != endCol) {
+            if (board[checkRow][checkCol].getElement() != null) {
+                // Il y a une pièce sur le chemin
+                return false;
+            }
+            checkRow += rowDirection;
+            checkCol += colDirection;
+        }
+
+        return true;
+    }
+    private boolean isDiagonalPathClear(int startRow, int startCol, int endRow, int endCol, Cell[][] board) {
+        int rowDirection = (endRow - startRow) / Math.abs(endRow - startRow);
+        int colDirection = (endCol - startCol) / Math.abs(endCol - startCol);
+
+        int checkRow = startRow + rowDirection;
+        int checkCol = startCol + colDirection;
+
+        while (checkRow != endRow && checkCol != endCol) {
+            if (board[checkRow][checkCol].getElement() != null) {
+                // Il y a une pièce sur le chemin
+                return false;
+            }
+            checkRow += rowDirection;
+            checkCol += colDirection;
+        }
+
+        return true;
+    }
 }
+
